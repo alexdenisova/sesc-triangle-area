0fbe75981f37c2e645164f7ef0e963358e2b7856
Alex Denisova <alexadenisova@gmail.com>
2018-09-24 16:38:38 +0700

    area

---
+++ area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float side;
+    scanf("%f", &side);
+    float sq = sqrtf(3);
+    float area = pow(side, 2) * sq /4;
+    printf("%.3f", area);
+}
